446e74c5fa4f57c84b16c4873b19474321a24f43
pagxir <pagxir@gmail.com>
2011-05-23 15:37:20 +0800

    add png filter support.

--- png2bmp.c
+++ png2bmp.c
@@ -9,26 +9,26 @@
 
 typedef struct 
 {
-    uint16_t bfType;
-    uint32_t bfSize;
-    uint16_t bfReserved1;
-    uint16_t bfReserved2;
-    uint32_t bfOffBits;
+	uint16_t bfType;
+	uint32_t bfSize;
+	uint16_t bfReserved1;
+	uint16_t bfReserved2;
+	uint32_t bfOffBits;
 }__attribute__((packed)) bitmap_file_header_t;
 
 typedef struct
 {
-    uint32_t biSize;
-    int32_t biWidth;
-    int32_t biHeight;
-    uint16_t biPlanes;
-    uint16_t biBitCount;
-    uint32_t biCompression;
-    uint32_t biSizeImage;
-    int32_t biXPelsPerMeter;
-    int32_t biYPelsPerMeter;
-    uint32_t biClrUsed;
-    uint32_t biClrImportant;
+	uint32_t biSize;
+	int32_t biWidth;
+	int32_t biHeight;
+	uint16_t biPlanes;
+	uint16_t biBitCount;
+	uint32_t biCompression;
+	uint32_t biSizeImage;
+	int32_t biXPelsPerMeter;
+	int32_t biYPelsPerMeter;
+	uint32_t biClrUsed;
+	uint32_t biClrImportant;
 } bitmap_info_header_t;
 
 struct png_header_t {
@@ -51,9 +51,10 @@ static char * png_src, * bmp_dst;
 static FILE * bmp_file = NULL;
 static struct png_header_t png_hdr;
 
-int png_tranafer_init(void);
-int png_tranafer_clean(void);
-int png_tranafer_dosave(const char * path);
+int png_transfer_init(void);
+int png_transfer_clean(void);
+int png_transfer_dosave(const char * path);
+int png_transfer_filter(char * data, size_t height, size_t width);
 
 int main(int argc, char *argv[])
 {
@@ -71,12 +72,12 @@ int main(int argc, char *argv[])
 
 	printf("\n\tFile Name : [%s]\n\n", path);
 	if (png_header_read(file)) {
-		png_tranafer_init();
-	   	png_chunks_list(file);
-		png_tranafer_dosave("kitty.bmp");
+		png_transfer_init();
+		png_chunks_list(file);
+		png_transfer_dosave("kitty.bmp");
 	}
 
-	png_tranafer_clean();
+	png_transfer_clean();
 	fclose(file);
 	return 0;
 }
@@ -106,7 +107,7 @@ int bitmap_getsize()
 	return (line & ~0x3) * png_hdr.height;
 }
 
-int png_tranafer_init(void)
+int png_transfer_init(void)
 {
 	int byteperpixel, byteperline;
 	byteperpixel = bitmap_byte_per_pixel(png_hdr.colortype, png_hdr.bitdepth);
@@ -121,7 +122,7 @@ int png_tranafer_init(void)
 	return 0;
 }
 
-int png_tranafer_dosave(const char * path)
+int png_transfer_dosave(const char * path)
 {
 	int err;
 	int byteperpixel;
@@ -158,9 +159,9 @@ int png_tranafer_dosave(const char * path)
 		int line;
 		char * data;
 
-	   	file = fopen(path, "wb");
-	   	fwrite(&hdr_file, sizeof(hdr_file), 1, file);
-	   	fwrite(&hdr_info, sizeof(hdr_info), 1, file);
+		file = fopen(path, "wb");
+		fwrite(&hdr_file, sizeof(hdr_file), 1, file);
+		fwrite(&hdr_info, sizeof(hdr_info), 1, file);
 
 		line = bitmap_byte_per_pixel(png_hdr.colortype, png_hdr.bitdepth);
 		line = line * png_hdr.width;
@@ -169,58 +170,164 @@ int png_tranafer_dosave(const char * path)
 		data = bmp_dst;
 		switch (byteperpixel) {
 			case 3:
-			   	for (i = 0; i < png_hdr.height; i++) {
-				   	for (j = 0; j < png_hdr.width; j++) {
+				for (i = 0; i < png_hdr.height; i++) {
+					data++;
+					for (j = 0; j < png_hdr.width; j++) {
 						char t = data[j * 3];
 						data[j * 3] = data[j * 3 + 2];
 						data[j * 3 + 2] = t;
 					}
-				   	data += (line + 1);
+					data += line;
 				}
 				break;
 
 			case 4:
-			   	for (i = 0; i < png_hdr.height; i++) {
-				   	for (j = 0; j < png_hdr.width; j++) {
+				png_transfer_filter(data, png_hdr.height, png_hdr.width);
+				for (i = 0; i < png_hdr.height; i++) {
+					data++;
+					for (j = 0; j < png_hdr.width; j++) {
 						char t = data[j * 4];
-						data[j * 4] = data[j * 4 + 3];
-						data[j * 4 + 3] = t;
-
-						t = data[j * 4 + 1];
-						data[j * 4 + 1] = data[j * 4 + 2];
+						data[j * 4] = data[j * 4 + 2];
 						data[j * 4 + 2] = t;
 					}
-
-				   	data += (line + 1);
+					data += line;
 				}
 				break;
 		}
 
 		data = bmp_dst;
 		for (i = 0; i < png_hdr.height; i++) {
+			data ++;
 			fwrite(data, 1, line & ~0x0003, file);
-			data += (line + 1);
+			data += line;
 		}
 
 		printf("ZLIB: %d, %d, %d, %d, %d\n",
-			   	err, png_hdr.width, png_hdr.height, len_dst, len_src);
+				err, png_hdr.width, png_hdr.height, len_dst, len_src);
 
 #if 0
 		FILE * datfile = fopen("a.out", "wb");
 		fwrite(png_src, len_src, 1, datfile);
 		fclose(datfile);
 #endif
-	   	fclose(file);
+		fclose(file);
 		return 0;
 	}
 
 	return 0;
 }
 
-int png_tranafer_clean(void)
+int png_transfer_clean(void)
 {
 	free(png_src);
 	free(bmp_dst);
+	return 0;
+}
+
+int paeth(int a, int b, int c)
+{
+	int p, pa, pb, pc;
+
+	p = (a + b - c);
+	pa = abs(p - a);
+	pb = abs(p - b);
+	pc = abs(p - c);
+
+	if (pa <= pb && pa <= pc)
+		return a;
+
+	if (pb <= pc)
+		return b;
+
+	return c;
+}
+
+int png_transfer_filter(char * data, size_t height, size_t width)
+{
+	int i, j;
+	int filter;
+	unsigned char *preline, *curline;
+	unsigned char *curpixel, *priorpixel;
+
+	curline = data;
+	preline = (curline + 1);
+	for (i = 0; i < height; i++) {
+		filter = *curline++;
+
+		switch (filter) {
+			case 0:
+				break;
+
+			case 1:
+				curpixel = (curline + 4);
+				for (j = 1; j < width; j++) {
+					*curpixel = *curpixel + *(curpixel - 4);
+					curpixel++;
+					*curpixel = *curpixel + *(curpixel - 4);
+					curpixel++;
+					*curpixel = *curpixel + *(curpixel - 4);
+					curpixel++;
+					*curpixel = *curpixel + *(curpixel - 4);
+					curpixel++;
+				}
+				break;
+
+			case 2:
+				curpixel = curline;
+				priorpixel = preline;
+				for (j = 0; j < width; j++) {
+					*curpixel = *curpixel + *priorpixel;
+					curpixel++, priorpixel++;
+					*curpixel = *curpixel + *priorpixel;
+					curpixel++, priorpixel++;
+					*curpixel = *curpixel + *priorpixel;
+					curpixel++, priorpixel++;
+					*curpixel = *curpixel + *priorpixel;
+					curpixel++, priorpixel++;
+				}
+				break;
+
+			case 4:
+				curpixel = curline;
+				priorpixel = preline;
+
+				*curpixel = *curpixel + paeth(0, *priorpixel, 0);
+				curpixel++, priorpixel++;
+				*curpixel = *curpixel + paeth(0, *priorpixel, 0);
+				curpixel++, priorpixel++;
+				*curpixel = *curpixel + paeth(0, *priorpixel, 0);
+				curpixel++, priorpixel++;
+				*curpixel = *curpixel + paeth(0, *priorpixel, 0);
+				curpixel++, priorpixel++;
+
+				for (j = 1; j < width; j++) {
+					*curpixel = *curpixel +
+						paeth(*(curpixel - 4), *priorpixel, *(priorpixel - 4));
+					curpixel++, priorpixel++;
+
+					*curpixel = *curpixel + 
+						paeth(*(curpixel - 4), *priorpixel, *(priorpixel - 4));
+					curpixel++, priorpixel++;
+
+					*curpixel = *curpixel +
+						paeth(*(curpixel - 4), *priorpixel, *(priorpixel - 4));
+					curpixel++, priorpixel++;
+
+					*curpixel = *curpixel +
+						paeth(*(curpixel - 4), *priorpixel, *(priorpixel - 4));
+					curpixel++, priorpixel++;
+				}
+				break;
+
+			default:
+				assert(0);
+				break;
+		}
+
+		preline = curline;
+		curline += (width * 4);
+	}
+
 	return 0;
 }
 
@@ -247,19 +354,19 @@ int png_header_read(FILE * file)
 	if (!fread(png_len, 8, 1, file)) {
 		return 0;
 	}
-   
+
 	if (memcmp(&png_len[1], "IHDR", 4)) {
 		return 0;
 	}
 
 	len = htonl(png_len[0]);
-   	if (len != sizeof(pnghdr) ||
-		   	!fread(&pnghdr, len, 1, file)) {
-	   	return 0;
+	if (len != sizeof(pnghdr) ||
+			!fread(&pnghdr, len, 1, file)) {
+		return 0;
 	}
 
 	if (!fread(&crc, sizeof(crc), 1, file)) {
-	   	return 0;
+		return 0;
 	}
 
 	printf("LENGTH:[%8d] TYPE:[%s] \n", len, &png_len[1]);
@@ -271,18 +378,18 @@ int png_header_read(FILE * file)
 int png_header_print(struct png_header_t * header, size_t len, int crc)
 {
 	if (len >= sizeof(png_hdr)) {
-	   	memcpy(&png_hdr, header, sizeof(png_hdr));
+		memcpy(&png_hdr, header, sizeof(png_hdr));
 		png_hdr.width = htonl(png_hdr.width);
 		png_hdr.height = htonl(png_hdr.height);
-	   	printf("\t---------- IHDR Image header ----------\n");
+		printf("\t---------- IHDR Image header ----------\n");
 		printf("\tCHUNK DATA\n");
 		printf("\t\tWidth              (4 bytes): [%d]\n", png_hdr.width);
-	   	printf("\t\tHeight             (4 bytes): [%d]\n", png_hdr.height);
+		printf("\t\tHeight             (4 bytes): [%d]\n", png_hdr.height);
 		printf("\t\tBitdepth           (1 bytes): [%u]\n", png_hdr.bitdepth);
-	   	printf("\t\tColor Type         (1 bytes): [%u]\n", png_hdr.colortype);
-	   	printf("\t\tCompression method (1 bytes): [%u]\n", png_hdr.comdivssion);
-	   	printf("\t\tFilter method      (1 bytes): [%u]\n", png_hdr.filter);
-	   	printf("\t\tInterlace method   (1 bytes): [%u]\n", png_hdr.interlace);
+		printf("\t\tColor Type         (1 bytes): [%u]\n", png_hdr.colortype);
+		printf("\t\tCompression method (1 bytes): [%u]\n", png_hdr.comdivssion);
+		printf("\t\tFilter method      (1 bytes): [%u]\n", png_hdr.filter);
+		printf("\t\tInterlace method   (1 bytes): [%u]\n", png_hdr.interlace);
 	}
 
 	printf("\tCRC : %08x\n", htonl(crc));
@@ -309,22 +416,24 @@ int png_chunk1_read(FILE * file)
 	char data[CHUNK];
 
 	if (fread(png_len, 8, 1, file) == 1) {
-	   	len = htonl(png_len[0]);
+		len = htonl(png_len[0]);
 		if (len <= 0 || !fread(data, len, 1, file))
-		   	return 0;
+			return 0;
 
-	   	if (fread(&crc, 4, 1, file) != 1)
-		   	return 0;
+		if (fread(&crc, 4, 1, file) != 1)
+			return 0;
 
 #if 1
 		printf("LENGTH:[%8d] TYPE:[%s] CRC:[%08x]\n",
-			   	len, &png_len[1], htonl(crc));
+				len, &png_len[1], htonl(crc));
 #endif
 
-		if (memcmp(&png_len[1], "IDAT", 4))
+		if (memcmp(&png_len[1], "IDAT", 4)) {
+			fprintf(stderr, "UNKOWN CHUNK %.4s\n", &png_len[1]);
 			return 1;
+		}
 
-	   	printf("CHUNK LENGTH: %d, CRC:[%08x]\n", len, htonl(crc));
+		printf("CHUNK LENGTH: %d, CRC:[%08x]\n", len, htonl(crc));
 
 #if 0
 		size_t dstlen = len_dst;
